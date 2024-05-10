22adda5e1f56eb881190532e8f34afc3bc7b77dd
Thierry Morre <thierry.morre@cognitic.be>
2024-05-10 14:40:24 +0200

    Ajout de commentaires

--- Models/Courant.cs
+++ Models/Courant.cs
@@ -4,4 +4,6 @@ public class Courant
 {
     //Ici sera implémenté les fonctionnalités de la classe Courant
     //Dans le cadre des exercices lié à C# OO
+
+    //Déclaration des variables membres
 }
