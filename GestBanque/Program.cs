b5f2a351272e4b994a01781c9766fb4cc6a820d2
Legrain Samuel <samuel.legrain@bstorm.be>
2024-05-13 15:11:08 +0200

    Correction Slide 33

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Personne p1 = new Personne();
+p1.Nom = "Legrain";
+p1.Prenom = "Samuel";
+p1.DateNaiss = new DateTime(1987, 9, 27);
+
+Personne p2 = new Personne() { 
+    Nom = "Morre",
+    Prenom = "Thierry",
+    DateNaiss = new DateTime(1970,1,1)
+};
+
+Console.WriteLine($"Identité : {p1.Nom} {p1.Prenom}, né le {p1.DateNaiss}");
+Console.WriteLine($"Identité : {p2.Nom} {p2.Prenom}, né le {p2.DateNaiss}");
+
+Courant c1 = new Courant() { 
+    Numero = "BE01",
+    LigneDeCredit = 200,
+    Titulaire = new Personne()
+    {
+        Nom = "Person",
+        Prenom = "Michael",
+        DateNaiss = new DateTime(1980,1,1)
+    }
+};
+
+c1.Depot(20_000);
+Console.WriteLine($"Le compte courant : {c1.Numero}\n" +
+    $"du titulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom}\n" +
+    $"né le {c1.Titulaire.DateNaiss}\n" + 
+    $"a comme solde {c1.Solde} € avec une ligne de credit de {c1.LigneDeCredit} €.");
+c1.Retrait(21_000);
--- Models/Courant.cs
+++ Models/Courant.cs
@@ -2,8 +2,38 @@
 
 public class Courant
 {
-    //Ici sera implémenté les fonctionnalités de la classe Courant
-    //Dans le cadre des exercices lié à C# OO
+	private double _ligneDeCredit;
 
-    //Déclaration des variables membres
+    public string Numero { get; set; }
+    public Personne Titulaire { get; set; }
+    public double Solde { get; private set; }
+	public double LigneDeCredit
+	{
+		get { return _ligneDeCredit; }
+		set {
+			if(value >= 0)
+			{
+				_ligneDeCredit = value;
+			}
+			/* Pour une application Console, c'est OK, mais pas pour les autres...
+				Donc on évite ce genre de code, et utilisons la gestion d'exceptions et d'événements
+			else
+			{
+                Console.WriteLine("Ligne de crédit en négatif...");
+            }*/
+		}
+	}
+
+	public void Depot(double montant)
+	{
+		if (montant <= 0) return;		//Gestion des exceptions
+		Solde += montant;
+	}
+
+	public void Retrait(double montant)
+	{
+		if (montant <= 0) return;							//Gestion des exceptions
+		if (montant > Solde + LigneDeCredit) return;		//Gestion des exceptions
+		Solde -= montant;
+    }
 }
